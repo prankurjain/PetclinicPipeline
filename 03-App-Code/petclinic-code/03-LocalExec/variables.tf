847b6d0c9fa7d4744f0eb8c215174066eb2574ac
root <root@ubu.zwab4uta5gquxcbwqaibpsqweh.cx.internal.cloudapp.net>
2021-06-24 10:41:26 +0000

    New Jenkin file as a Jenkins1

---
+++ 03-App-Code/petclinic-code/03-LocalExec/variables.tf
@@ -0,0 +1,18 @@
+variable "key_name" {
+  default = "terraform-demo-301"
+}
+
+variable "pvt_key_name" {
+  default = "/root/.ssh/my-key.pem"
+}
+
+
+variable "sg_id" {
+  default = "sg-010af018771d38228"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
+
+
